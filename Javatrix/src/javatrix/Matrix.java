30a6735173cebfa20e6d3bd508670e9635c90022
Kanthalon <kanthalon@gmail.com>
2016-02-12 13:34:29 -0500

    Fix missing files

---
+++ Javatrix/src/javatrix/Matrix.java
@@ -0,0 +1,19 @@
+package javatrix;
+
+public class Matrix {
+
+	private double[][] matArray;
+	
+	/**
+	 * @param matArray
+	 */
+	public Matrix(double[][] matArray) {
+		double[][] array = new double[matArray.length][matArray[0].length];
+		for (int i = 0; i < matArray.length; i++)
+			for (int j = 0; j < matArray[0].length; i++)
+		this.matArray = array;
+	}
+
+	
+	
+}
