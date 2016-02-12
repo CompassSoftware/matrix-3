21fe0d621926b673cec703862a7e41a469b6aa1a
walkers2 <walkers2@CS-ABH309-21.appd.appstate.edu>
2016-02-12 13:26:43 -0500

    2D constructor

--- javatrix/src/javatrix/Matrix.java
+++ javatrix/src/javatrix/Matrix.java
@@ -2,4 +2,15 @@ package javatrix;
 
 public class Matrix {
 
+	private double[][] matArray;
+	
+	/**
+	 * @param matArray
+	 */
+	public Matrix(double[][] matArray) {
+		this.matArray = matArray;
+	}
+
+	
+	
 }
